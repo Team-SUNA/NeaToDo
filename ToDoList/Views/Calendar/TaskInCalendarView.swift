88b0df2e210070438ef1922971b816d2f70d2486
nayeon <vennie20@naver.com>
2022-09-26 18:46:00 +0900

    well done, no task 구분

--- ToDoList/Views/Calendar/TaskInCalendarView.swift
+++ ToDoList/Views/Calendar/TaskInCalendarView.swift
@@ -13,47 +13,42 @@ struct TaskInCalendarView: View {
     @State private var selectedTask: Task? = nil
     @EnvironmentObject var realmManager: RealmManager
     var tasks: [Task] { return realmManager.tasks.filter({ task in
-        return isSameDay(date1: task.taskDate, date2: currentDate) && !task.isCompleted
+        return isSameDay(date1: task.taskDate, date2: currentDate)
     })}
     
-
     var body: some View {
         if !tasks.isEmpty {
-            ScrollView {
-                ForEach(tasks, id: \.self) { task in
-                    HStack {
-                        Capsule()
-                            .fill(Color.black)
-                            .frame(width: 5, height: 30)
-                        Text(task.taskTitle)
-                            .font(.system(size: 20.0, weight: .semibold))
-                        Spacer()
-                        // for custom timing
-                        Text(task.taskDate, style: .time)
-                            .font(.system(size: 15.0))
+            let notDoneTask = tasks.filter({ task in return !task.isCompleted})
+            if !notDoneTask.isEmpty {
+                ScrollView {
+                    ForEach(notDoneTask, id: \.self) { task in
+                        HStack {
+                            Capsule()
+                                .fill(Color.black)
+                                .frame(width: 5, height: 30)
+                            Text(task.taskTitle)
+                                .font(.system(size: 20.0, weight: .semibold))
+                            Spacer()
+                            // for custom timing
+                            Text(task.taskDate, style: .time)
+                                .font(.system(size: 15.0))
+                        }
+                        .background(.white)
+                        .onTapGesture {
+                            selectedTask = task
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                    .background(.white) // 클릭위해서
-                    .onTapGesture {
-                        selectedTask = task
+                    .sheet(item: $selectedTask) {
+                        UpdateModalView(task: $0)
+                            .environmentObject(realmManager)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
-                .sheet(item: $selectedTask) {
-                    UpdateModalView(task: $0)
-                                .environmentObject(realmManager)
-                }
+            } else {
+                Text("WELL DONE")
             }
         } else {
             Text("NO TASK TO DO")
         }
     }
 }
-
-//struct TaskInCalendarView_Previews: PreviewProvider {
-//    @State static var date = Date()
-//
-//    static var previews: some View {
-//        TaskInCalendarView(currentDate: $date)
-//            .environmentObject(RealmManager())
-//    }
-//}
